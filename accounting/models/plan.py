509bd966f437ab5fc9f63368c9c4bb686080c596
jai <iaver17omar@gmail.com>
2023-02-10 00:49:55 -0300

    [CREATE] plan model

---
+++ accounting/models/plan.py
@@ -0,0 +1,45 @@
+from djando.db import models
+from django_fsm import FSMField, transition, State
+from ..defines import PLAN_STATES, DRAFT, CREATED, PUBLISHED, HIDDEN, FINISHED
+
+class Plan(models.Model):
+    # State machine
+    state = FSMField(
+      default=DRAFT,
+      choices=PLAN_STATES,
+      verbose_name='Plan state',
+      protected=True)
+    # Fields
+    name = models.CharField(max_length=100)
+    kind = models.CharField(max_length=50)
+    company_quantities = models.IntegerField()
+
+    # Timestamps
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Plan'
+        verbose_name_plural = 'Plans'
+    
+    def __str__(self):
+        return self.name
+    
+    @transition(field=state, source=DRAFT, target=CREATED)
+    def create(self):
+        pass
+
+    @transition(field=state, source=CREATED, target=PUBLISHED)
+    def publish(self):
+        pass
+
+    @transition(field=state, source=PUBLISHED, target=HIDDEN)
+    def hide(self):
+        pass
+    
+    @transition(field=state, source=HIDDEN, target=FINISHED)
+    def finish(self):
+        pass
+  
+
+    
