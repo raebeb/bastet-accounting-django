b28dd6adf728dc1a6d6956bbf4b1be0f197438d0
Fran <raebeb@gmail.com>
2023-03-12 20:06:27 -0300

    wip

--- accounting/models/company.py
+++ accounting/models/company.py
@@ -6,9 +6,10 @@ class Company(models.Model):
     Company model
     """
     name = models.CharField('name', max_length=50)
-    #TODO: Change this name to something more meaningful
-    tax_refered = models.CharField('tax refered', max_length=50)
-    address = models.CharField('address', max_length=50)
+    tax_refered = models.CharField('tax refered', max_length=50) #rut
+    #TODO: encrypt password
+    password = models.CharField('password', max_length=200, null=True, blank=True)
+
     class Meta:
         verbose_name = "Company"
         verbose_name_plural = "Companies"
