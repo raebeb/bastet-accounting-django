42b36922fb0f255dd188caa38bc065d4f19d0911
Fran <raebeb@gmail.com>
2023-03-16 15:45:24 -0300

    ADD permission classes to endpoint

--- accounting/views/company.py
+++ accounting/views/company.py
@@ -5,6 +5,7 @@ from rest_framework import permissions
 from rest_framework import status
 from rest_framework.response import Response
 
+from rest_framework.permissions import IsAuthenticated
 from accounting.models import Company
 from accounting.serializers.company_serializer import CompanySerializer
 from rest_framework.viewsets import GenericViewSet
@@ -19,7 +20,7 @@ import pdb # for debugging
 class CompanyViewSet(GenericViewSet, CreateModelMixin, ListModelMixin, RetrieveModelMixin, UpdateModelMixin):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [IsAuthenticated]
 
     def get_serializer_class(self):
         return CompanySerializer
