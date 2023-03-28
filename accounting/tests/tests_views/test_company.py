515e33935038a0035de2965c9f759c7066717ce0
jai <iaver17omar@gmail.com>
2023-03-27 22:37:50 -0300

    CREATE: autentincation helper wiith jwt ADD: test for fail create
    Co-authored-by: Fran <raebeb@users.noreply.github.com>

--- accounting/tests/tests_views/test_company.py
+++ accounting/tests/tests_views/test_company.py
@@ -5,6 +5,7 @@ from rest_framework.test import APITestCase
 from .test_utils import CustomAPITestCase
 from accounting.models import Company
 
+from pdb import set_trace as st
 class CompanyViewSetTest(CustomAPITestCase):
     def setUp(self):
       super().setUp()
@@ -15,9 +16,13 @@ class CompanyViewSetTest(CustomAPITestCase):
         """
         Test that we can create a new Company object using the API
         """
-
         response = self.client.post(self.url, data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Company.objects.count(), 1)
-        self.assertEqual(Company.objects.get().organization, self.organization)
+
+    def test_create_fail(self):
+        data = { }
+        self.url = reverse('company-list')
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
